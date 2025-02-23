4237631d76fcf762b16eb358673d96e915495321
cjwattttts <cameron.jwatts@outlook.com>
2025-02-23 01:18:25 -0500

    Task 2 - Created Manager Class with Inheritance

--- cc_9.js
+++ cc_9.js
@@ -23,3 +23,27 @@ console.log(emp1.getDetails());
 
 console.log(emp1.calculateAnnualSalary()); 
 // Expected output: 60000
+
+//TASK 2 
+class Manager extends Employee {
+    constructor(name, id, department, salary, teamSize) {
+        super(name, id, department, salary); // calls parent consutructor
+        this.teamSize = teamSize; // sets the team size 
+    }
+
+    getDetails() { // includes teamSize in getDetails
+        return `Manager: ${this.name}, ID: ${this.id}, Department: ${this.department}, Salary: $${this.salary}, Team Size: ${this.teamSize}`;
+    }
+
+    calculateBonus() { // calculates 10 percent bonus of salary
+        return this.calculateAnnualSalary() * 0.1;
+    }
+}
+
+// Test cases
+const mgr1 = new Manager("John Smith", 201, "IT", 8000, 5);
+console.log(mgr1.getDetails());
+// Expected output: "Manager: John Smith, ID: 201, Department: IT, Salary: $8000, Team Size: 5"
+
+console.log(mgr1.calculateBonus()); 
+// Expected output: 9600
